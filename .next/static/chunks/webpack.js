1c00cec4763eb1855b64868b902061631b898977
Alin Antoci <alin.georgian.antoci@gmail.com>
2022-01-17 12:28:25 +0200

    Feature section done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0914d539943b6570fa0a";
+/******/ 	var hotCurrentHash = "b228ca919e7cd87a10a1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
