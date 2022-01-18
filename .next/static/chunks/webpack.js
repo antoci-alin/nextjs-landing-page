bad82cc1bf390f1807efa42c5468fff3c6688ab0
Alin Antoci <alin.georgian.antoci@gmail.com>
2022-01-18 12:46:54 +0200

    Footer done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9f152996e6ea2199b401";
+/******/ 	var hotCurrentHash = "aa33cb0097dcbca913e4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
